a2f79575536ab92d45833931e7e658ac5b1a90a2
Lucia Acuña <luciaacunaaaa@gmail.com>
2025-06-04 16:01:03 -0300

    Ejercicio Falso Login

---
+++ javascript/falsoLogin.js
@@ -0,0 +1,6 @@
+const Usuario = document.getElementById("Usuario").value;
+const Contraseña = document.getElementById("Contraseña").value;
+const button = document.getElementById("Botón").value
+
+const usu = "Lupu";
+const contr = "0905";
